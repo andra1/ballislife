7f0bcb97e746e222760c9260fbb77154e0c1ba91
andra1 <kaushik.andra92@gmail.com>
2018-02-19 17:04:33 -0600

    third version

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,7 +7,28 @@ class GamesController < ApplicationController
                  :start_time => params["start_time"],
                  :end_time => params["end_time"]
     redirect_to "/games"
+  end
 
+  def update
+  	game = Game.find_by(id: params["id"])
+  	game.name = params["name"]
+  	game.location = params["location"]
+    game.max_players = params["max_players"]
+    game.start_time = params["start_time"]
+    game.end_time = params["end_time"]
+    game.save
+
+    redirect_to "/games"
   end  
+
+
+  def destroy
+  	game = Game.find_by(id: params["id"])
+  	game.delete
+  	redirect_to "/games"
+  	
+
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,25 +3,14 @@ Rails.application.routes.draw do
   root 'games#index'
 
   get "/games" => "games#index"
+
   get "/games/new" => 'games#new'
   post "/games" => 'games#create'
 
-  get "games/show" => 'games#show'
+  get "/games/:id" => 'games#edit'
+  patch "/games/:id" => 'games#update'
 
-  get "/movies/:id" => 'movies#show'
-  delete "/movies/:id" => 'movies#destroy'
-  get "/movies/:id/edit" => 'movies#edit'
-  patch "/movies/:id" => 'movies#update'
-
-  get "/directors" => "directors#index"
-  get "/directors/new" => 'directors#new'
-  post "/directors" => 'directors#create'
-
-  get "/directors/:id" => 'directors#show'
-  delete "/directors/:id" => 'directors#destroy'
-  get "/directors/:id/edit" => 'directors#edit'
-  patch "/directors/:id" => 'directors#update'
-
+  delete "/games/:id" => 'games#destroy'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
